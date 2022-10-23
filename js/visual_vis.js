14f02c1aa857d6d22803310d5eab5eb657647a0a
n00bK <115654602+n00bK@users.noreply.github.com>
2022-10-24 02:08:24 +0800

    Final Visualization update

---
+++ js/visual_vis.js
@@ -0,0 +1,14 @@
+var spec1 = "js/stacked_bar.vg.json"
+vegaEmbed('#stacked_bar', spec1).then(function(result) {}).catch(console.error);
+
+var spec2 = "js/scatter_plot.vg.json";
+vegaEmbed('#scatter_plot', spec2).then(function(result) {}).catch(console.error);
+
+var spec3 = "js/symbol.vg.json";
+vegaEmbed('#symbol_plot', spec3).then(function(result) {}).catch(console.error);
+
+var spec4 = "js/symbol2.vg.json";
+vegaEmbed('#symbol2_plot', spec4).then(function(result) {}).catch(console.error);
+
+var spec5 = "js/streamgraph.vg.json";
+vegaEmbed('#stream_plot', spec5).then(function(result) {}).catch(console.error);
